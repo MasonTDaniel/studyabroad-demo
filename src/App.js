e2341cdac9611b6be1b5ee1d8907982b22693c4f
Mason <masondanielt@gmail.com>
2020-04-24 16:56:03 -0500

    routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom'
 import { Security, SecureRoute, LoginCallback } from '@okta/okta-react'
 import './App.css';
 import './css/bootstrap.min.css';
@@ -29,16 +29,18 @@ const config = {
 class App extends Component {
   render() {
     return (
-      <Router basename="/studyabroad-demo">
-        <Security {...config}>
-          <NavBar />
-          <div className="container">
-            <Route exact path="/" component={Home} />
-            <Route path='/implicit/callback' component={LoginCallback} />
-            <SecureRoute exact path="/admin-dashboard" component={AdminDashboard} />
-          </div>
-        </Security>
-      </Router>
+      <Switch>
+        <Router basename="/studyabroad-demo">
+          <Security {...config}>
+            <NavBar />
+            <div className="container">
+              <Route exact path="/" component={Home} />
+              <Route path='/implicit/callback' component={LoginCallback} />
+              <SecureRoute exact path="/admin-dashboard" component={AdminDashboard} />
+            </div>
+          </Security>
+        </Router>
+      </Switch>
     );
   }
 }
