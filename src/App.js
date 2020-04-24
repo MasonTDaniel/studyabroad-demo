7df32941d81cb6f5a71217bf813b86ba8e3fc7c4
Mason <masondanielt@gmail.com>
2020-04-24 17:01:46 -0500

    routing

--- src/App.js
+++ src/App.js
@@ -34,9 +34,9 @@ class App extends Component {
           <Security {...config}>
             <NavBar />
             <div className="container">
-              <Route exact path="/" component={Home} />
-              <Route path='/implicit/callback' component={LoginCallback} />
-              <SecureRoute exact path="/admin-dashboard" component={AdminDashboard} />
+              <Route exact path="process.env.PUBLIC_URL + '/'" component={Home} />
+              <Route path="process.env.PUBLIC_URL + '/implicit/callback'" component={LoginCallback} />
+              <SecureRoute exact path="process.env.PUBLIC_URL + '/admin-dashboard'" component={AdminDashboard} />
             </div>
           </Security>
         </Router>
