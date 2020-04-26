b0d009b290628533f613fe811feebda2f3a78637
Mason <masondanielt@gmail.com>
2020-04-25 21:33:32 -0500

    removed more auth errors

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,10 +8,10 @@ const NavBar = () => {
 
     /* Variable to store information on whether user is logged in or not
        Uses Okta's provided useOktaAuth method to determine this  */
-    const { authState, authService } = useOktaAuth();
+    //const { authState, authService } = useOktaAuth();
     /* Whenever the const logout is invoked, we will log the user out by calling the function authService.logout()
       e.g. When the user clicks 'Log out', we will invoke authService.logout() function provided by Okta  */
-    const logout = async () => authService.logout('/');
+    // const logout = async () => authService.logout('/');
 
     return (
         <div id="nav" className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 border-bottom shadow-sm">
@@ -20,7 +20,7 @@ const NavBar = () => {
             <a className="btn" href="https://www.southwestern.edu/study-abroad/contact-us/" target="_blank">Contact Us</a>
             <NavLink to="/admin-dashboard" className="btn" >Admin</NavLink>
             {/* Only display the logout tag in the navbar when an admin user is logged in */}
-            {authState.isAuthenticated && <button style={{ "cursor": "pointer", "marginLeft": "5px", "marginRight": "14px", "color": "#212529" }} onClick={logout}>Log Out</button>}
+            {/*{authState.isAuthenticated && <button style={{ "cursor": "pointer", "marginLeft": "5px", "marginRight": "14px", "color": "#212529" }} onClick={logout}>Log Out</button>}*/}
         </div>
     );
 }
