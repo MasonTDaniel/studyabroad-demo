f5afce3479ca813d00c14598a3eb7cf6e9a6070b
Mason <masondanielt@gmail.com>
2020-04-26 18:45:52 -0500

    added logo in nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 //import { useOktaAuth } from '@okta/okta-react';
 import { NavLink, Link } from 'react-router-dom'
-import { Logo } from '../img/su-icon.png'
+import Logo from '../img/su-logo.png'
 
 /* A basic navbar with tabs Browse, How To, Contact Us, Admin, and Log Out (only if admin user is logged in */
 const NavBar = () => {
@@ -15,7 +15,7 @@ const NavBar = () => {
 
     return (
         <div id="nav" className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 border-bottom shadow-sm">
-            <Link to="/" className="btn my-0 mr-md-auto align-self-start img-fluid "><img src={Logo} alt="" /></Link>
+            <Link to="/" className="my-0 mr-md-auto align-self-start"><img style={{ "width": "15rem" }} src={Logo} alt="" /></Link>
             <NavLink to="/" className="btn" >Search</NavLink>
             <a className="btn" href="https://masontdaniel.github.io" target="_blank" rel="noopener noreferrer">Contact Us</a>
             <NavLink to="/admin-dashboard" className="btn" >Admin</NavLink>
