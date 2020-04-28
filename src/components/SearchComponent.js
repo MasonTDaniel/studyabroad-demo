9371f94358dc276bcd7bd01ff3ee6fcac94a1c0c
Mason <masondanielt@gmail.com>
2020-04-27 19:32:26 -0500

    added demo tag

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -38,6 +38,7 @@ class SearchComponent extends Component {
     render() {
         return (
             <div>
+                <h5 style={{ "textAlign": "center", "color": "gray" }}>*(Frontend Demo)</h5>
                 <h5 style={{ "textAlign": "center" }}>Search for study abroad programs recommended by SU below</h5>
                 <SearchBar state={this.state} />
             </div>
